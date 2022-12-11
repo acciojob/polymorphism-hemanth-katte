1ebd029428ed220737f84c0af73169d35ecc2ead
Hemanth Katte <hemanthss24@gmail.com>
2022-12-11 20:20:40 +0530

    polymorphism demonstration

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,33 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public int product(int x, int y){
+            return x + y;
+        }
+
+        public int product(int x, int y, int z){
+            return x + y + z;
+        }
+
+        public double product(double x, double y){
+            return x - y;
+        }
+    }
+
+    public static void main(String[] args){
+        Product p = new Product();
+        int x  = 14;
+        int y = 21;
+        int z = 11;
+        double a = 954562352;
+        double b = 984562;
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+        //System.out.println(ans);
+
+
+
+    }
 }
