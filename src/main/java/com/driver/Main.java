b74ee6ccdd277ed2f1ee323275c6e135e92a4aae
Hemanth Katte <hemanthss24@gmail.com>
2022-12-11 20:39:59 +0530

    modified product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
     public static class Product{
         public int product(int x, int y){
-            return x + y;
+            return x * y;
         }
 
         public int product(int x, int y, int z){
-            return x + y + z;
+            return x * y * z;
         }
 
         public double product(double x, double y){
-            return x - y;
+            return x * y;
         }
     }
 
     public static void main(String[] args){
         Product p = new Product();
-        int x  = 14;
-        int y = 21;
-        int z = 11;
-        double a = 954562352;
-        double b = 984562;
+        int x  = 1;
+        int y = 2;
+        int z = 3;
+        double a = 1.2;
+        double b = 1.4;
         int res1 = p.product(x,y);
         int res2 = p.product(x,y,z);
         double res3 = p.product(a,b);
